e5628782b092f0f55b0a7b8cdddaa50722cfd34e
EndePointe <endepointe@gmail.com>
2020-06-15 02:57:30 +0100

    cors issues

--- server/app.js
+++ server/app.js
@@ -4,12 +4,15 @@ const app = express();
 const path = require('path');
 const email = require('./routes/email');
 const port = process.env.port || 3001;
-
-app.use(cors());
+var corsOptions = {
+  origin: 'http://localhost:4001/',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // middelware
-//app.use('/send-email', email);
+app.use('/send-email', email);
 
 // PRODUCTION //
 ///*
