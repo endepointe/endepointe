3de1564f7e9201679aed9133b50c682b767b71d6
EndePointe <endepointe@gmail.com>
2020-06-16 22:57:53 +0100

    ssdd

--- server/routes/email.js
+++ server/routes/email.js
@@ -1,10 +1,19 @@
 const nodemailer = require('nodemailer');
+const pass = require('../emailCredentials');
 const router = require('express').Router();
 
 // setup email in a bit
 
 router.post('/', async (req, res) => {
-  console.log(nodemailer);
+  console.log(pass.EMAILPASS, 'ul');
+  let mailTransporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'endepointe@gmail.com',
+      pass: pass.EMAILPASS,
+      subject: 'test'
+    }
+  })
   res.send('reached email')
 });
 
