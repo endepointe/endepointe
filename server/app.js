5fd741e1de549aa72ec2aeab86dae1fcb4d787fe
EndePointe <endepointe@gmail.com>
2020-06-18 22:16:23 +0100

    email status set

--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ app.use('/send-email', email);
 ///*
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 //*/
