e6bf00e0e97a300b7a8f23968f15a5a8f1798682
endepointe <endepointe@gmail.com>
2021-05-11 15:01:04 -0700

    ssdd

--- navbar_3.js
+++
@@ -1,5 +0,0 @@
-const menuBtn = document.getElementById('navbar_3_menu_btn');
-menuBtn.addEventListener('click', () => {
-  console.log(document.getElementById('navbar_3_menu_list').classList);
-  document.getElementById('navbar_3_menu_list').classList.toggle('show');
-});
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+
+export default function Navbar() {
+  return (
+    <nav className="flex flex-row flex-nowrap h-9">
+      <div className="flex-1">
+        <h3>ENDE</h3>
+        <h3>POINTE</h3>
+      </div>
+      <ul className="flex-2">
+        <li>
+          <a className="">Projects</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
