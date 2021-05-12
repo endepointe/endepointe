58db2f9f7f80b53fd310fd4010cca2b73f9aabeb
endepointe <endepointe@gmail.com>
2021-05-12 12:24:11 -0700

    logo name position

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,19 @@
 
 export default function Navbar() {
   return (
-    <nav className="flex flex-row flex-nowrap h-9">
-      <div className="flex-1">
-        <h3>ENDE</h3>
-        <h3>POINTE</h3>
+    <nav className="flex flex-row flex-nowrap">
+      <div
+        id="logo"
+        className="flex-2 justify-between">
+        <h3
+          className="px-1 text-5xl font-black"
+          id="ende">ENDE</h3>
+        <h3
+          id="pointe"
+          className="transform rotate-180 text-5xl leading-none px-1">POINTE</h3>
       </div>
-      <ul className="flex-2">
-        <li>
+      <ul className="flex-1">
+        <li className="flex justify-end">
           <a href="#"
             className="">Projects</a>
         </li>
